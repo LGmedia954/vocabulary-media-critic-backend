0be578ea9846b380c3bfc42d261150047031710b
LGmedia954 <LeslieGonzalez@me.com>
2021-11-10 13:13:02 -0500

    tested associations

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,9 @@
 class Sentence < ApplicationRecord
+  belongs_to :vocabulary_word
+  # validates :example, :presence => {:message => "Please enter a sentence using this word."}
+  # validates :example, :length => {:maximum => 150, :message => "Limit 150 characters."}
+
+  #Obscenity gem is no longer being maintained. Basic filtering added below. RIP comedian George Carlin.
+
+  #validates :sentence, exclusion: { in: %w(shit piss fuck cunt cocksucker motherfucker tits), message: "Wash your mouth out with soap!" }
 end
--- app/models/vocabulary_word.rb
+++ app/models/vocabulary_word.rb
@@ -1,2 +1,6 @@
 class VocabularyWord < ApplicationRecord
+  has_many :sentences
+  # validates :word, presence: true
+  # validates :part_of_speech, presence: true
+  # validates :definition, presence: true
 end
