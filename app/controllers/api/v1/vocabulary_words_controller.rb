f05ce968f306c2d6c095d9850798596f66e2edb5
LGmedia954 <LeslieGonzalez@me.com>
2021-11-10 13:47:32 -0500

    routes and vocab controller

---
+++ app/controllers/api/v1/vocabulary_words_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::VocabularyWordsController < ApplicationController
+  
+end
---
+++ test/controllers/api/v1/vocabulary_words_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::VocabularyWordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
