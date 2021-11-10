e939adbb2edeed4f13004f270c2413b4f8f92ae7
LGmedia954 <LeslieGonzalez@me.com>
2021-11-10 14:23:24 -0500

    vocab words controller index and create routes

--- app/controllers/api/v1/vocabulary_words_controller.rb
+++ app/controllers/api/v1/vocabulary_words_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::VocabularyWordsController < ApplicationController
+
+  def index
+    @vocabulary = VocabularyWord.all
+    render json: @vocabulary
+  end
+
+  def create
+    vocabulary_word = VocabularyWord.new(vocabulary_word_params)
+    if vocabulary_word.save
+      render json: vocabulary_word, status: :accepted
+    else
+      ender json: {errors: vocabulary_word.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def vocabulary_word_params
+    params.require(:vocabulary_word).permit(:word, :part_of_speech, :definition, :v_word_id)
   
+  end
+
 end
