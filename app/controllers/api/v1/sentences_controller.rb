7fd630c3a8fbb9427270b9b59b22b359188cfcab
LGmedia954 <LeslieGonzalez@me.com>
2021-11-10 22:17:30 -0500

    sentences_controller and routes

---
+++ app/controllers/api/v1/sentences_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::SentencesController < ApplicationController
+
+  def index
+    @sentences = Sentence.all
+    # render json: @sentences
+    render json: SentenceSerializer.new(@sentences)
+  end
+
+  def create
+    sentence = Sentence.new(sentence_params)
+    if sentence.save
+      render json: sentence, status: :accepted
+    else
+      render json: {errors: sentence.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def sentence_params
+    params.require(:sentence).permit(:example, :vocabulary_word_id)
+  
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :vocabulary_words, only: [:index, :create]
-      # resources :sentences
+      resources :sentences, only: [:index, :create]
     end
   end
   
