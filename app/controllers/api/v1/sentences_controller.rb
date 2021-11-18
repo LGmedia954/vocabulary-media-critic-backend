a4e3fd86135e417261ccda0e7a53ce9295d799b0
LGmedia954 <LeslieGonzalez@me.com>
2021-11-18 13:40:35 -0500

    create methods

--- app/controllers/api/v1/sentences_controller.rb
+++ app/controllers/api/v1/sentences_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::SentencesController < ApplicationController
   def create
     sentence = Sentence.new(sentence_params)
     if sentence.save
-      render json: Sentence.new(sentence), status: :accepted
+      render json: SentenceSerializer.new(sentence), status: :accepted
     else
       render json: {errors: sentence.errors.full_messages}, status: :unprocessible_entity
     end
